86c2c0518b39d3b2da67447d7cda67bbc2621390
Devin Gonzalez <devin@labs.epiuse.com>
2012-05-24 19:14:59 -0400

    Enable User editing and JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html {render}
+      format.json {render :json => @user}
+    end
   end
 
   def new
@@ -15,6 +20,38 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+
+      respond_to do |format|
+        format.html {redirect_to @user}
+        format.json {@user.errors.full_messages.each 
+          do |message|
+            render :text => message
+          end
+        }
+      end
+
+      #render :text => 'success'##
+    else
+      respond_to do |format|
+        format.html {render 'edit'}
+        format.json {@user.errors.full_messages.each 
+          do |message|
+            render :text => message
+          end
+        }
+      end
     end
   end
 
